a4a8e2e1b454302e10e7fefbebdbcca3d1b16dbb
Juan Pablo Yagüe <juanpabloyague@gmail.com>
2022-10-15 16:36:52 -0500

    Finishing

---
+++ src/js/main.js
@@ -0,0 +1,49 @@
+const url = "http://localhost:4000/movements";
+
+const balance = document.querySelector(".balance__graphics");
+
+let arr = [];
+
+eventListeners();
+function eventListeners() {
+  window.addEventListener("DOMContentLoaded", showMovements);
+}
+
+function showMovements() {
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => showInfo(data));
+}
+
+function showInfo(data) {
+  data.forEach((d) => {
+    const { day, amount } = d;
+
+    const balanceContainer = document.createElement("div");
+    balanceContainer.classList.add("balance__graphic-container");
+
+    const balanceGraphic = document.createElement("div");
+    balanceGraphic.classList.add("balance__graphic");
+    balanceGraphic.style.height = `${amount * 2}px`;
+    balanceGraphic.dataset.title = `$${amount}`;
+
+    const balanceText = document.createElement("p");
+    balanceText.classList.add("balance__graphic-text");
+    balanceText.textContent = day;
+
+    balanceContainer.append(balanceGraphic);
+    balanceContainer.append(balanceText);
+
+    balance.append(balanceContainer);
+
+    arr.push(parseFloat(balanceGraphic.style.height));
+  });
+  let root = document.documentElement;
+  const max = Math.max(...arr);
+  const elements = document.querySelectorAll(".balance__graphic");
+  elements.forEach((element) => {
+    if (element.style.height === `${max}px`) {
+      element.style.backgroundColor = "hsl(186, 34%, 60%)";
+    }
+  });
+}
